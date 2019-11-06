611eb8dae6b57b7b2059252e432bfc04086756fa
jparacha <jparacha@wsjp.objectiveit.de>
2019-11-06 08:59:43 +0100

    added

--- src/main/java/com/example/boardofdirectorsServer/api/CalculationController.java
+++ src/main/java/com/example/boardofdirectorsServer/api/CalculationController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.example.boardofdirectorsServer.model.Entry;
 
-@RequestMapping
+@RequestMapping("api/v1/calculation")
 @RestController
 public class CalculationController {
 	
