d459dbd5e9b186657f990deb21af33efd9fe77b0
junaid paracha <junaidp@gmail.com>
2019-11-07 07:06:17 +0100

    excel reader

--- src/main/java/com/example/boardofdirectorsServer/BoardofdirectorsServerApplication.java
+++ src/main/java/com/example/boardofdirectorsServer/BoardofdirectorsServerApplication.java
@@ -1,8 +1,13 @@
 package com.example.boardofdirectorsServer;
 
+import java.io.IOException;
+
+import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.CrossOrigin;
+
+import com.example.boardofdirectorsServer.api.Calculation;
 
 @SpringBootApplication
 @CrossOrigin(origins = "*")
@@ -10,6 +15,17 @@ public class BoardofdirectorsServerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BoardofdirectorsServerApplication.class, args);
+		try {
+			try {
+				Calculation c = new Calculation();
+			} catch (InvalidFormatException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 
 }
--- src/main/java/com/example/boardofdirectorsServer/api/Calculation.java
+++ src/main/java/com/example/boardofdirectorsServer/api/Calculation.java
@@ -1,7 +1,70 @@
 package com.example.boardofdirectorsServer.api;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.HashMap;
+
+import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
+import org.apache.poi.openxml4j.opc.OPCPackage;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.CellType;
+import org.apache.poi.ss.usermodel.FormulaEvaluator;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+
+
 public class Calculation {
 
 	
 	///ifrs.xls
+	
+	public Calculation() throws IOException, InvalidFormatException
+	{
+	//	FileInputStream fis = new FileInputStream("/Users/junaidparacha/Downloads/ifrs.xlsx");
+		OPCPackage pkg = OPCPackage.open(new File("/Users/junaidparacha/Downloads/ifrs.xlsx"));
+	//	Workbook wb = new XSS(fis); //or new XSSFWorkbook("/somepath/test.xls")
+		XSSFWorkbook wb = new XSSFWorkbook(pkg);
+		
+		FormulaEvaluator evaluator = wb.getCreationHelper().createFormulaEvaluator();
+		HashMap<String, HashMap<String, String>> map = new HashMap<String, HashMap<String, String>>();
+		for (Sheet sheet : wb) {
+			HashMap<String, String> mapSheet = new HashMap<String, String>();
+			
+			for (Row r : sheet) {
+		        for (Cell c : r) {
+		        	System.out.println(c.getCellType());
+		        	if (c.getCellType() == CellType.FORMULA) {
+		        		try{
+		        		evaluator.evaluateFormulaCell(c);
+		        		}catch(Exception ex){
+		        			System.out.println(ex);
+		        		}
+		                
+		        		switch(c.getCachedFormulaResultType()) {
+		                case NUMERIC:
+		                    System.out.println("Last evaluated as: " + c.getNumericCellValue());
+		                    mapSheet.put(""+c.getRowIndex()+"/"+c.getColumnIndex()+"", c.getNumericCellValue()+"");
+		                    map.put(sheet.getSheetName(), mapSheet);
+		                    break;
+		                case STRING:
+		                    System.out.println("Last evaluated as \"" + c.getRichStringCellValue() + "\"");
+		                    mapSheet.put(""+c.getRowIndex()+"/"+c.getColumnIndex()+"", c.getRichStringCellValue()+"");
+		                    map.put(sheet.getSheetName(), mapSheet);
+		                    break;
+						default:
+							break;
+		            }
+		        		
+		            }
+		        }
+		    }
+		}
+		
+		System.out.println("RESULT>>>>>>>>>>>>>>>>>>>>>>>>>>>>");
+		System.out.println(map);
+	}
 }
