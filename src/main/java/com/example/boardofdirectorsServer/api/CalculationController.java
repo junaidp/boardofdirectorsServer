04469b1e88a8b01ce980077c74c2f9ad9536839c
junaid paracha <junaidp@gmail.com>
2019-11-08 21:33:39 +0100

    final3

--- src/main/java/com/example/boardofdirectorsServer/api/CalculationController.java
+++ src/main/java/com/example/boardofdirectorsServer/api/CalculationController.java
@@ -5,6 +5,7 @@ import java.util.HashMap;
 import java.util.LinkedHashMap;
 
 import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,24 +17,33 @@ import com.example.boardofdirectorsServer.model.Entry;
 @RestController
 public class CalculationController {
 	
+	public  LinkedHashMap<String, LinkedHashMap<String, String>> mapAnswer ;
+	
 	@PostMapping
-	public HashMap<String, HashMap<String, String>> calculate(@RequestBody Entry entry)
+	public LinkedHashMap<String, LinkedHashMap<String, String>> calculate(@RequestBody Entry entry)
 	{
 		
 		try {
 			Calculation c = new Calculation();
 			LinkedHashMap map = c.entry(entry);
+			mapAnswer = map;
 			return map;
 			
 		} catch (InvalidFormatException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+			 e.printStackTrace();
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 		return  null;
 		
+	}
+	
+	@GetMapping
+	public LinkedHashMap<String, LinkedHashMap<String, String>> getCalculation(){
+		if(mapAnswer != null)
+		return mapAnswer;
+		return null;
 	}
 	
 //	@Autowired
