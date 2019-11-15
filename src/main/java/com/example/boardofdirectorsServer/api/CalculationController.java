1bf03dac6dfacc4bbfbc571cce83cb46edd8848d
jparacha <jparacha@wsjp.objectiveit.de>
2019-11-15 16:56:18 +0100

    dd

--- src/main/java/com/example/boardofdirectorsServer/api/CalculationController.java
+++ src/main/java/com/example/boardofdirectorsServer/api/CalculationController.java
@@ -1,9 +1,12 @@
 package com.example.boardofdirectorsServer.api;
 
 import java.io.IOException;
+import java.util.List;
 
 import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -19,53 +22,61 @@ import com.example.boardofdirectorsServer.model.UserRepository;
 @RestController
 @CrossOrigin(origins = "*", maxAge = 3600)
 public class CalculationController {
-	
+
 	String json;
-	
+
 	@PostMapping
 	public String calculate(@RequestBody Entry entry) throws Exception
 	{
-		
+
 		try {
 			Calculation c = new Calculation();
-			 json = c.entry(entry);
-			
+			json = c.entry(entry);
+
 			return json;
-			
+
 		} catch (InvalidFormatException e) {
 			throw e;
 		} catch (IOException e) {
 			throw e;
 		}
-		
+
 	}
-	
+
 	@PostMapping("/lease")
 	public String calculateLease(@RequestBody Entry entry) throws Exception
 	{
 		try {
 			Calculation c = new Calculation();
-			 json = c.entryLease(entry);
-			 return json;
+			json = c.entryLease(entry);
+			return json;
 		} catch (Exception e) {
 			throw e;
 		}
 	}
-	
+
 	@GetMapping
 	public String getCalculation(){
-		
+
 		return json;
 	}
-	
+
 	@Autowired
 	UserRepository userRepository;
-	
+
 	@PostMapping("/saveUser")
-	public String saveEntry(@RequestBody User user)
+	public String saveUser(@RequestBody User user)
 	{
-		
+
 		userRepository.save(user);
 		return "user saved";		
-}
+	}
+	
+	@GetMapping("/getUser")
+	public User singIn(@RequestBody String userName, String password)
+	{
+		User user = userRepository.findUserByName(userName);
+		return user;
+	}
+	
 }
--- src/main/java/com/example/boardofdirectorsServer/model/UserRepository.java
+++ src/main/java/com/example/boardofdirectorsServer/model/UserRepository.java
@@ -1,9 +1,14 @@
 package com.example.boardofdirectorsServer.model;
 
+import java.util.List;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
+
 
 public interface UserRepository extends MongoRepository<User, String>{
 	
-	
+	@Query("{'name : ?0'}")
+	User findUserByName(String name);
 
 }
