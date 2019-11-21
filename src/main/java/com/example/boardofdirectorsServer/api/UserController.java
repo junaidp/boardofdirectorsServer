54c0771a0e966c3e08675c532e8c647f88336cb9
jparacha <jparacha@wsjp.objectiveit.de>
2019-11-21 12:46:40 +0100

    h

--- src/main/java/com/example/boardofdirectorsServer/api/UserController.java
+++ src/main/java/com/example/boardofdirectorsServer/api/UserController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.example.boardofdirectorsServer.model.User;
 
-@RequestMapping("users")
+@RequestMapping("/users")
 @RestController
 @CrossOrigin(origins = "*", maxAge = 3600)
 public class UserController {
