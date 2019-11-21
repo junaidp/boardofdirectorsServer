7cc2b2917138283316c295b707aa260efa99b298
jparacha <jparacha@wsjp.objectiveit.de>
2019-11-21 12:23:03 +0100

    h

--- src/main/java/com/example/boardofdirectorsServer/api/UserController.java
+++ src/main/java/com/example/boardofdirectorsServer/api/UserController.java
@@ -1,4 +1,4 @@
-/*package com.example.boardofdirectorsServer.api;
+package com.example.boardofdirectorsServer.api;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -16,26 +16,26 @@ import com.example.boardofdirectorsServer.model.User;
 public class UserController {
 	
 	@Autowired
-	UserHelper user;
+	//UserHelper user;
 	
 	@PostMapping("/saveUser")
 	public String saveUser(@RequestBody User userEntity)
 	{
 
-		return user.saveUser(userEntity);
+		return "user.saveUser(userEntity)";
 	}
 	
 	@GetMapping("/getUser")
 	public String singIn(@RequestBody String userName, String password) throws Exception
 	{
-		return user.getUser(userName, password);
+		return "user.getUser(userName, password)";
 		
 	}
 	
 	@GetMapping("/getAllUsers")
 	public String getAllUsers()
 	{
-		return user.getAllUsers();
+		return "user.getAllUsers()";
 	}
 
-}*/
+}
