a88282b72f5275dde9d893eb214369d5aa201ffc
jparacha <jparacha@wsjp.objectiveit.de>
2019-11-28 13:03:42 +0100

    d

--- src/main/java/com/example/boardofdirectorsServer/model/UserTest.java
+++ src/main/java/com/example/boardofdirectorsServer/model/UserTest.java
@@ -1,10 +1,20 @@
 package com.example.boardofdirectorsServer.model;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 public class UserTest {
 	
 	
 	private String name;
 	private String password;
+	
+	public UserTest(@JsonProperty("name")String name, @JsonProperty("password")String password)
+	{
+		this.name = name;
+		this.password = password;
+	}
+	
+	
 	public String getName() {
 		return name;
 	}
