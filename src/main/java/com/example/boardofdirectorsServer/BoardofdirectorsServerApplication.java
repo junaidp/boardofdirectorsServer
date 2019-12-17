30aff2efcac8f5081204b5bd6dc42125811cbbf2
jparacha <jparacha@wsjp.objectiveit.de>
2019-12-17 08:25:11 +0100

    x

--- src/main/java/com/example/boardofdirectorsServer/BoardofdirectorsServerApplication.java
+++ src/main/java/com/example/boardofdirectorsServer/BoardofdirectorsServerApplication.java
@@ -1,38 +1,46 @@
 package com.example.boardofdirectorsServer;
 
+import java.io.IOException;
+import java.util.Date;
+
+import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.CrossOrigin;
+
+import com.example.boardofdirectorsServer.api.Calculation;
+import com.example.boardofdirectorsServer.model.Entry;
+import com.google.gson.Gson;
 
 @SpringBootApplication
 @CrossOrigin(origins = "*")
 public class BoardofdirectorsServerApplication {
 
 	public static void main(String[] args) throws Exception {
-		
+	
 		SpringApplication.run(BoardofdirectorsServerApplication.class, args);
-		/*
-		try {
-			try {
-			//	Calculation c = new Calculation();
-				Entry entry = new Entry("Lease No. 1", new Date("05/10/2019"), "Ending", 7, 60, 60, 250000, "Monthly", 0, 200000, 10, 15, 3, 2019);
-			//	c.entry(entry);
-				
-				Calculation c = new Calculation();
-				String map = c.entryJournal(entry);
-				Gson gson = new Gson(); 
-				String json = gson.toJson(map);
-			//	System.out.println(json);
-				
-			} catch (InvalidFormatException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-	*/
+//			
+//		try {
+//			try {
+//			//	Calculation c = new Calculation();
+//				Entry entry = new Entry("Lease No. 1", new Date("05/10/2019"), "Ending", 7, 60, 60, 250000, "Monthly", 0, 200000, 10, 15, 3, 2019);
+//			//	c.entry(entry);
+//				
+//				Calculation c = new Calculation();
+//				String map = c.entryJournal(entry);
+//				Gson gson = new Gson(); 
+//				String json = gson.toJson(map);
+//			//	System.out.println(json);
+//				
+//			} catch (InvalidFormatException e) {
+//				// TODO Auto-generated catch block
+//				e.printStackTrace();
+//			}
+//		} catch (IOException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		}
+	
 	}
 
 }
