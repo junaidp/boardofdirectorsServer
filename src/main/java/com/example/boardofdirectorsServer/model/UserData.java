3c33b57739b5da8a864ac40033944cde1a8cede1
jparacha <jparacha@wsjp.objectiveit.de>
2019-12-20 12:17:03 +0100

    userData

--- src/main/java/com/example/boardofdirectorsServer/model/UserData.java
+++ src/main/java/com/example/boardofdirectorsServer/model/UserData.java
@@ -10,6 +10,21 @@ public class UserData {
 	@Id
 	private String dataId;
 	private String userId;
+	private String leaseContractNo;
+	private String classAsset;
+	private String commencementDate;
+	private String paymentsAt;
+	private String annualDiscountRate;
+	private String leaseTerm;
+	private String expectedPeriod;
+	private String leasePayment;
+	private String paymentIntervals;
+	private String initialDirectCost;
+	private String guaranteedResidualValue;
+	private String usefulLifeOfTheAsset;
+	private String escalation;
+	private String escalationAfterEvery;
+	
 	
 	public String getDataId() {
 		return dataId;
@@ -23,9 +38,92 @@ public class UserData {
 	public void setUserId(String userId) {
 		this.userId = userId;
 	}
+	public String getLeaseContractNo() {
+		return leaseContractNo;
+	}
+	public void setLeaseContractNo(String leaseContractNo) {
+		this.leaseContractNo = leaseContractNo;
+	}
+	public String getClassAsset() {
+		return classAsset;
+	}
+	public void setClassAsset(String classAsset) {
+		this.classAsset = classAsset;
+	}
+	public String getCommencementDate() {
+		return commencementDate;
+	}
+	public void setCommencementDate(String commencementDate) {
+		this.commencementDate = commencementDate;
+	}
+	public String getPaymentsAt() {
+		return paymentsAt;
+	}
+	public void setPaymentsAt(String paymentsAt) {
+		this.paymentsAt = paymentsAt;
+	}
+	public String getAnnualDiscountRate() {
+		return annualDiscountRate;
+	}
+	public void setAnnualDiscountRate(String annualDiscountRate) {
+		this.annualDiscountRate = annualDiscountRate;
+	}
+	public String getLeaseTerm() {
+		return leaseTerm;
+	}
+	public void setLeaseTerm(String leaseTerm) {
+		this.leaseTerm = leaseTerm;
+	}
+	public String getExpectedPeriod() {
+		return expectedPeriod;
+	}
+	public void setExpectedPeriod(String expectedPeriod) {
+		this.expectedPeriod = expectedPeriod;
+	}
+	public String getLeasePayment() {
+		return leasePayment;
+	}
+	public void setLeasePayment(String leasePayment) {
+		this.leasePayment = leasePayment;
+	}
+	public String getPaymentIntervals() {
+		return paymentIntervals;
+	}
+	public void setPaymentIntervals(String paymentIntervals) {
+		this.paymentIntervals = paymentIntervals;
+	}
+	public String getInitialDirectCost() {
+		return initialDirectCost;
+	}
+	public void setInitialDirectCost(String initialDirectCost) {
+		this.initialDirectCost = initialDirectCost;
+	}
+	public String getGuaranteedResidualValue() {
+		return guaranteedResidualValue;
+	}
+	public void setGuaranteedResidualValue(String guaranteedResidualValue) {
+		this.guaranteedResidualValue = guaranteedResidualValue;
+	}
+	public String getUsefulLifeOfTheAsset() {
+		return usefulLifeOfTheAsset;
+	}
+	public void setUsefulLifeOfTheAsset(String usefulLifeOfTheAsset) {
+		this.usefulLifeOfTheAsset = usefulLifeOfTheAsset;
+	}
+	public String getEscalation() {
+		return escalation;
+	}
+	public void setEscalation(String escalation) {
+		this.escalation = escalation;
+	}
+	public String getEscalationAfterEvery() {
+		return escalationAfterEvery;
+	}
+	public void setEscalationAfterEvery(String escalationAfterEvery) {
+		this.escalationAfterEvery = escalationAfterEvery;
+	}
 	
-	
-	
+
 
 	
 }
