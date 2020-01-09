13a7ccb1a5a026c7a7f2cca6acb9c48c7e8f75fa
junaid paracha <junaidp@gmail.com>
2020-01-09 20:25:21 +0100

    monthly change

--- src/main/java/com/example/boardofdirectorsServer/api/CalculationController.java
+++ src/main/java/com/example/boardofdirectorsServer/api/CalculationController.java
@@ -101,7 +101,7 @@ public class CalculationController {
 	{
 		try {
 			Calculation c = new Calculation();
-			json = c.entryJournal(entry, TYPES.JOURNAL_MONTHLY, TYPES.LEASE_YEARLY);
+			json = c.entryJournal(entry, TYPES.JOURNAL_MONTHLY, TYPES.LEASE_MONTHLY);
 			return json;
 		} catch (Exception e) {
 			throw e;
