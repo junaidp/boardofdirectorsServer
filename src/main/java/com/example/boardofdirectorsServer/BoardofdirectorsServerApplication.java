fd33d9276d65b12c39129b1b81a97c26a74d7579
junaid paracha <junaidp@gmail.com>
2020-01-26 20:19:20 +0100

    d

--- src/main/java/com/example/boardofdirectorsServer/BoardofdirectorsServerApplication.java
+++ src/main/java/com/example/boardofdirectorsServer/BoardofdirectorsServerApplication.java
@@ -31,7 +31,7 @@ public class BoardofdirectorsServerApplication {
 		//	c.entry(entry);
 			
 			Calculation c = new Calculation();
-			String map = c.entryJournal(entry, TYPES.JOURNAL_YEARLY, TYPES.LEASE_YEARLY);
+			String map = c.entryJournal(entry, TYPES.JOURNAL_QUARTERLY, TYPES.LEASE_QUARTERLY);
 			Gson gson = new Gson(); 
 			String json = gson.toJson(map);
 		//	System.out.println(json);
