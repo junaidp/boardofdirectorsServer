fa752b5487a7080c7598284479c79ca46a770ebc
junaid paracha <junaidp@gmail.com>
2020-02-13 15:25:26 +0100

    fix

--- src/main/java/com/example/boardofdirectorsServer/api/CalculationController.java
+++ src/main/java/com/example/boardofdirectorsServer/api/CalculationController.java
@@ -1,7 +1,12 @@
 package com.example.boardofdirectorsServer.api;
 
 import java.io.IOException;
+import java.util.LinkedHashMap;
+import java.util.List;
+
 import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -9,13 +14,18 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.boardofdirectorsServer.helper.DataHelper;
 import com.example.boardofdirectorsServer.model.Entry;
+import com.example.boardofdirectorsServer.model.UserData;
+import com.google.gson.Gson;
 
 @RequestMapping("calculation")
 @RestController
 @CrossOrigin(origins = "*", maxAge = 3600)
 public class CalculationController {
 
+	@Autowired
+	DataHelper dataHelper;
 	String json;
 
 	@PostMapping
@@ -72,7 +82,7 @@ public class CalculationController {
 		}
 	}
 	
-	@PostMapping("/journal/yearly")
+	/*@PostMapping("/journal/yearly")
 	public String calculateJournalYearly(@RequestBody Entry entry) throws Exception
 	{
 		try {
@@ -82,15 +92,16 @@ public class CalculationController {
 		} catch (Exception e) {
 			throw e;
 		}
-	}
+	}*/
 	
-	/*@PostMapping("/journal/yearly")
+	@PostMapping("/journal/yearly")
 	public String calculateJournalYearly(@RequestBody Entry entry) throws Exception
 	{
 		try {
 			Calculation c = new Calculation();
 			
 			///
+			Gson gson = new Gson();
 			int userId = entry.getUserId();
 			List<UserData> dataList = dataHelper.getUserData(userId+"");
 			///
@@ -112,7 +123,7 @@ public class CalculationController {
 		} catch (Exception e) {
 			throw e;
 		}
-	}*/
+	}
 
 
 	@PostMapping("/journal/quarterly")
